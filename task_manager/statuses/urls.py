dc92245c43266721b009fff4c5c564eb98c8c6fd
root <root@DESKTOP-FILLTED>
2024-10-23 21:05:06 +0300

    fix22

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import StatusListView, StatusCreateView,\
-    StatusUpdateView, StatusDeleteView
+from .views import StatusListView, StatusCreateView, StatusUpdateView, StatusDeleteView
 
 
 urlpatterns = [
-    path('', StatusListView.as_view(), name='statuses'),
-    path('create/', StatusCreateView.as_view(), name='status_create'),
-    path('<int:pk>/update/', StatusUpdateView.as_view(), name='status_update'),
-    path('<int:pk>/delete/', StatusDeleteView.as_view(), name='status_delete'),
+    path("", StatusListView.as_view(), name="statuses"),
+    path("create/", StatusCreateView.as_view(), name="status_create"),
+    path("<int:pk>/update/", StatusUpdateView.as_view(), name="status_update"),
+    path("<int:pk>/delete/", StatusDeleteView.as_view(), name="status_delete"),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -3,8 +3,11 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
 
-from task_manager.mixins import MyLoginRequiredMixin,\
-    SelfCheckUserMixin, CanDeleteProtectedEntityMixin
+from task_manager.mixins import (
+    MyLoginRequiredMixin,
+    SelfCheckUserMixin,
+    CanDeleteProtectedEntityMixin,
+)
 from .models import MyUser
 from .forms import UserCreateForm, UserUpdateForm
 
@@ -12,51 +15,56 @@ from .forms import UserCreateForm, UserUpdateForm
 class UserListView(ListView):
     model = MyUser
     template_name = "users/list.html"
-    context_object_name = 'users'
+    context_object_name = "users"
     extra_context = {
-        'header': _('Users'),
+        "header": _("Users"),
     }
 
 
 class UserCreateView(SuccessMessageMixin, CreateView):
     model = MyUser
     form_class = UserCreateForm
-    template_name = 'form.html'
-    success_url = reverse_lazy('login')
-    success_message = _('User is created successfully')
+    template_name = "form.html"
+    success_url = reverse_lazy("login")
+    success_message = _("User is created successfully")
     extra_context = {
-        'header': _('Registration'),
-        'button_text': _('Register '),
+        "header": _("Registration"),
+        "button_text": _("Register "),
     }
 
 
-class UserUpdateView(MyLoginRequiredMixin, SelfCheckUserMixin,
-                     SuccessMessageMixin, UpdateView):
-    template_name = 'form.html'
+class UserUpdateView(
+    MyLoginRequiredMixin, SelfCheckUserMixin, SuccessMessageMixin, UpdateView
+):
+    template_name = "form.html"
     model = MyUser
     form_class = UserUpdateForm
-    success_url = reverse_lazy('users')
-    success_message = _('User is successfully updated')
-    permission_message = _('You have no rights to change another user.')
-    permission_url = reverse_lazy('users')
+    success_url = reverse_lazy("users")
+    success_message = _("User is successfully updated")
+    permission_message = _("You have no rights to change another user.")
+    permission_url = reverse_lazy("users")
     extra_context = {
-        'header': _('Update user'),
-        'button_text': _('Update'),
+        "header": _("Update user"),
+        "button_text": _("Update"),
     }
 
 
-class UserDeleteView(MyLoginRequiredMixin, SelfCheckUserMixin,
-                     CanDeleteProtectedEntityMixin,
-                     SuccessMessageMixin, DeleteView):
-    template_name = 'delete.html'
+class UserDeleteView(
+    MyLoginRequiredMixin,
+    SelfCheckUserMixin,
+    CanDeleteProtectedEntityMixin,
+    SuccessMessageMixin,
+    DeleteView,
+):
+    template_name = "delete.html"
     model = MyUser
-    success_url = reverse_lazy('users')
-    success_message = _('User is successfully deleted')
-    permission_message = _('You have no rights to change another user.')
-    permission_url = reverse_lazy('users')
-    protected_message = _('Unable to delete a user because he is in use')
-    protected_url = reverse_lazy('users')
+    success_url = reverse_lazy("users")
+    success_message = _("User is successfully deleted")
+    permission_message = _("You have no rights to change another user.")
+    permission_url = reverse_lazy("users")
+    protected_message = _("Unable to delete a user because he is in use")
+    protected_url = reverse_lazy("users")
     extra_context = {
-        'header': _('Deleting user'),
-        'button_text': _('Yes, delete'),
+        "header": _("Deleting user"),
+        "button_text": _("Yes, delete"),
     }

b9298dd84a630d921632ad7194368ab487a6896e
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-24 13:38:02 +0300

    fix after review

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, UserLoginView, UserLogoutView
+from .views import HomeView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),

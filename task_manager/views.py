ed23357b8eabf92efddc3dd1fb44aa7a36116c31
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-24 13:25:11 +0300

    fix files after review

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,30 +1,27 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import TemplateView
 
 
-def home(request):
-    return render(request, 'home.html')
+class HomeView(TemplateView):
+    template_name = "home.html"
 
 
 class UserLoginView(SuccessMessageMixin, LoginView):
-    template_name = 'form.html'
+    template_name = "form.html"
     form_class = AuthenticationForm
-    next_page = reverse_lazy('home')
     extra_context = {
-        'header': _('Login'),
-        'button_text': _('Enter'),
+        "header": _("Login"),
+        "button_text": _("Enter"),
     }
-    success_message = _('You are logged in')
+    success_message = _("You are logged in")
 
 
 class UserLogoutView(LogoutView):
-    next_page = reverse_lazy('home')
-
     def dispatch(self, request, *args, **kwargs):
-        messages.info(request, _('You are logged out'))
+        messages.info(request, _("You are logged out"))
         return super().dispatch(request, *args, **kwargs)

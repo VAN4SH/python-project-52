33f537e9baa19ef7c1fd13d4a01d62cb60873896
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-24 13:56:41 +0300

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -158,6 +158,7 @@ LOCALE_PATH = (os.path.join(BASE_DIR, 'locale'),)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

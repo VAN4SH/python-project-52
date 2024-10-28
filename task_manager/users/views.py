ba2ed87e9bd57be294477b7907b7367064aaf0fb
root <root@DESKTOP-FILLTED>
2024-10-28 17:25:32 +0300

    fix /users/views.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
+
 from task_manager.mixins import (
     MyLoginRequiredMixin,
     SelfCheckUserMixin,
@@ -11,52 +12,41 @@ from .models import MyUser
 from .forms import UserCreateForm, UserUpdateForm
 
 
-class HeaderButtonMixin:
-    header = ""
-    button_text = ""
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.header:
-            context["header"] = self.header
-        if self.button_text:
-            context["button_text"] = self.button_text
-        return context
-
-
-class UserListView(ListView, HeaderButtonMixin):
+class UserListView(ListView):
     model = MyUser
     template_name = "users/list.html"
     context_object_name = "users"
-    header = _("Users")
+    extra_context = {
+        "header": _("Users"),
+    }
 
 
-class UserCreateView(SuccessMessageMixin, CreateView, HeaderButtonMixin):
+class UserCreateView(SuccessMessageMixin, CreateView):
     model = MyUser
     form_class = UserCreateForm
     template_name = "form.html"
     success_url = reverse_lazy("login")
     success_message = _("User is created successfully")
-    header = _("Registration")
-    button_text = _("Register")
+    extra_context = {
+        "header": _("Registration"),
+        "button_text": _("Register "),
+    }
 
 
 class UserUpdateView(
-    MyLoginRequiredMixin,
-    SelfCheckUserMixin,
-    SuccessMessageMixin,
-    UpdateView,
-    HeaderButtonMixin,
+    MyLoginRequiredMixin, SelfCheckUserMixin, SuccessMessageMixin, UpdateView
 ):
+    template_name = "form.html"
     model = MyUser
     form_class = UserUpdateForm
-    template_name = "form.html"
     success_url = reverse_lazy("users")
     success_message = _("User is successfully updated")
     permission_message = _("You have no rights to change another user.")
     permission_url = reverse_lazy("users")
-    header = _("Update user")
-    button_text = _("Update")
+    extra_context = {
+        "header": _("Update user"),
+        "button_text": _("Update"),
+    }
 
 
 class UserDeleteView(
@@ -65,15 +55,16 @@ class UserDeleteView(
     CanDeleteProtectedEntityMixin,
     SuccessMessageMixin,
     DeleteView,
-    HeaderButtonMixin,
 ):
-    model = MyUser
     template_name = "delete.html"
+    model = MyUser
     success_url = reverse_lazy("users")
     success_message = _("User is successfully deleted")
     permission_message = _("You have no rights to change another user.")
     permission_url = reverse_lazy("users")
-    protected_message = _("Unable to delete a user because they are in use")
+    protected_message = _("Unable to delete a user because he is in use")
     protected_url = reverse_lazy("users")
-    header = _("Deleting user")
-    button_text = _("Yes, delete")
+    extra_context = {
+        "header": _("Deleting user"),
+        "button_text": _("Yes, delete"),
+    }

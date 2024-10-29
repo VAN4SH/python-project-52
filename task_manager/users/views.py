d3dcdb58a984485e2c08a6b208deff3fb098a95d
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-29 08:29:07 +0300

    edit users/views.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -16,9 +16,11 @@ class UserListView(ListView):
     model = MyUser
     template_name = "users/list.html"
     context_object_name = "users"
-    extra_context = {
-        "header": _("Users"),
-    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["header"] = _("Users")
+        return context
 
 
 class UserCreateView(SuccessMessageMixin, CreateView):
@@ -27,6 +29,12 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     template_name = "form.html"
     success_url = reverse_lazy("login")
     success_message = _("User is created successfully")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["header"] = _("Registration")
+        context["button_text"] = _("Register")
+        return context
 
 
 class UserUpdateView(
@@ -39,6 +47,12 @@ class UserUpdateView(
     success_message = _("User is successfully updated")
     permission_message = _("You have no rights to change another user.")
     permission_url = reverse_lazy("users")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["header"] = _("Update user")
+        context["button_text"] = _("Update")
+        return context
 
 
 class UserDeleteView(
@@ -56,7 +70,9 @@ class UserDeleteView(
     permission_url = reverse_lazy("users")
     protected_message = _("Unable to delete a user because he is in use")
     protected_url = reverse_lazy("users")
-    extra_context = {
-        "header": _("Deleting user"),
-        "button_text": _("Yes, delete"),
-    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["header"] = _("Deleting user")
+        context["button_text"] = _("Yes, delete")
+        return context

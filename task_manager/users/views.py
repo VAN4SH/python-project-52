b27ed80d0a6fa70cacfbc9f135fcc18fb7b64451
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-29 09:04:34 +0300

    edit users/views.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -16,11 +16,9 @@ class UserListView(ListView):
     model = MyUser
     template_name = "users/list.html"
     context_object_name = "users"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["header"] = _("Users")
-        return context
+    extra_context = {
+        "header": _("Users"),
+    }
 
 
 class UserCreateView(SuccessMessageMixin, CreateView):
@@ -29,12 +27,10 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     template_name = "form.html"
     success_url = reverse_lazy("login")
     success_message = _("User is created successfully")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["header"] = _("Registration")
-        context["button_text"] = _("Register")
-        return context
+    extra_context = {
+        "header": _("Registration"),
+        "button_text": _("Register "),
+    }
 
 
 class UserUpdateView(
@@ -47,12 +43,10 @@ class UserUpdateView(
     success_message = _("User is successfully updated")
     permission_message = _("You have no rights to change another user.")
     permission_url = reverse_lazy("users")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["header"] = _("Update user")
-        context["button_text"] = _("Update")
-        return context
+    extra_context = {
+        "header": _("Update user"),
+        "button_text": _("Update"),
+    }
 
 
 class UserDeleteView(
@@ -70,9 +64,7 @@ class UserDeleteView(
     permission_url = reverse_lazy("users")
     protected_message = _("Unable to delete a user because he is in use")
     protected_url = reverse_lazy("users")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["header"] = _("Deleting user")
-        context["button_text"] = _("Yes, delete")
-        return context
+    extra_context = {
+        "header": _("Deleting user"),
+        "button_text": _("Yes, delete"),
+    }

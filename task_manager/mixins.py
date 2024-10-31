17a8cf29b2e7d36503cbe8f2074785cca0f2e6cd
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-31 10:17:27 +0300

    edit mixins.py

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,7 +1,6 @@
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.utils.translation import gettext as _
 

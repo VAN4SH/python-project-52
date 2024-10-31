af40e54a13615eb751953318062e89b11b94c11d
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-31 10:13:05 +0300

    edit mixins.py

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -10,9 +10,8 @@ class MyLoginRequiredMixin(LoginRequiredMixin):
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request,
-                           _('You are not logged in! Please log in.'))
-            return redirect(reverse_lazy('login'))
+            messages.error(request, _("You are not logged in! Please log in."))
+            return redirect(reverse_lazy("login"))
 
         return super().dispatch(request, *args, **kwargs)
 
@@ -33,12 +32,26 @@ class CanDeleteProtectedEntityMixin:
     protected_message = None
     protected_url = None
 
+    def has_related_objects(self, obj):
+        for rel in obj._meta.get_fields():
+            if rel.one_to_many or rel.one_to_one:
+                accessor_name = rel.get_accessor_name()
+                related_queryset = getattr(obj, accessor_name)
+                if related_queryset.exists():
+                    return True
+            elif rel.many_to_many:
+                related_manager = getattr(obj, rel.name)
+                if related_manager.exists():
+                    return True
+        return False
+
     def post(self, request, *args, **kwargs):
-        try:
-            return super().post(request, *args, **kwargs)
-        except ProtectedError:
+        obj = self.get_object()
+        if self.has_related_objects(obj):
             messages.error(request, self.protected_message)
             return redirect(self.protected_url)
+        else:
+            return super().post(request, *args, **kwargs)
 
 
 class AuthorCanDeleteTaskMixin(UserPassesTestMixin):

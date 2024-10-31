40f67d5e110acb2c557c89f098668844a300f2dc
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-31 15:25:37 +0300

    edit statuses

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -54,7 +54,7 @@ class StatusDeleteView(MyLoginRequiredMixin, SuccessMessageMixin, DeleteView):
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
-        if self.object.statuses.exists():
+        if Task.objects.filter(status=self.object).exists():
             messages.error(request, self.error_message)
             return redirect(self.success_url)
         else:

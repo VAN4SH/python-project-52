0d275adc863b33382688c4473800e705f61bb83e
Dmitriev Ivan <dmitrievivan434@gmail.com>
2024-10-31 15:10:53 +0300

    edit statuses

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -2,7 +2,8 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.messages.views import SuccessMessageMixin
-
+from django.contrib import messages
+from django.shortcuts import redirect
 from task_manager.mixins import MyLoginRequiredMixin
 from .models import Status
 from .forms import StatusForm
@@ -41,21 +42,20 @@ class StatusUpdateView(MyLoginRequiredMixin, SuccessMessageMixin, UpdateView):
 
 
 class StatusDeleteView(MyLoginRequiredMixin, SuccessMessageMixin, DeleteView):
-    template_name = "delete.html"
+    template_name = 'delete.html'
     model = Status
-    success_url = reverse_lazy("statuses")
-    success_message = _("Status is successfully deleted")
-    protected_message = _("Unable to delete a status because it is in use")
-    protected_url = reverse_lazy("statuses")
+    success_url = reverse_lazy('statuses')
+    success_message = _('Status is successfully deleted')
+    error_message = _('Unable to delete a status because it is in use')
     extra_context = {
-        "header": _("Delete status"),
-        "button_text": _("Yes, delete"),
+        'header': _('Delete status'),
+        'button_text': _('Yes, delete'),
     }
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
-        if Task.objects.filter(status=self.object).exists():
-            messages.error(request, self.protected_message)
-            return redirect(self.protected_url)
+        if self.object.statuses.exists():
+            messages.error(request, self.error_message)
+            return redirect(self.success_url)
         else:
             return super().delete(request, *args, **kwargs)
